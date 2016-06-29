1e198c96b62f98750a0cd3a9becf0fc4ab3aa2e2
Timo Stollenwerk <tisto@plone.org>
2016-06-29 13:56:42 +0200

    Fix tslint violation.

--- src/components/home.ts
+++ src/components/home.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
 
 @Component({
-  selector: 'my-home',
+  selector: "my-home",
   template: `
     <h2>Home Component</h2>
     <p>This is the home component which can be found in src/components/home.ts</p>

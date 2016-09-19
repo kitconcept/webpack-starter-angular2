db2651860d2914874e529ec5648a1a4ef8e06504
Victor Fernandez de Alba <sneridagh@gmail.com>
2016-09-19 13:53:06 +0200

    Fix build by get rid of missing environment directory

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import '@angular/router';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);

7b6228af3e6dc5a3bf15828f182e996dc6e030c0
Timo Stollenwerk <tisto@plone.org>
2017-01-22 19:49:40 +0100

    Fix tslint for webpack 2.2.

--- webpack.config.js
+++ webpack.config.js
@@ -33,47 +33,18 @@ module.exports = {
     ],
     // noParse: [path.join(__dirname, 'node_modules', 'angular2', 'bundles')]
   },
-  // more options in the optional tslint object
-  // tslint: {
-  //   configuration: {
-  //     rules: {
-  //       quotemark: [true, "double"]
-  //     }
-  //   },
-
-  //   // tslint errors are displayed by default as warnings
-  //   // set emitErrors to true to display them as errors
-  //   emitErrors: false,
-
-  //   // tslint does not interrupt the compilation by default
-  //   // if you want any file with tslint errors to fail
-  //   // set failOnHint to true
-  //   failOnHint: true,
-
-  //   // These options are useful if you want to save output to files
-  //   // for your continuous integration server
-  //   fileOutput: {
-  //     // The directory where each file's report is saved
-  //     dir: "./tslint-report/",
-
-  //     // The extension to use for each report's filename. Defaults to "txt"
-  //     ext: "xml",
-
-  //     // If true, all files are removed from the report directory at the beginning of run
-  //     clean: true,
-
-  //     // A string to include at the top of every report file.
-  //     // Useful for some report formats.
-  //     header: "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<checkstyle version=\"5.7\">",
-
-  //     // A string to include at the bottom of every report file.
-  //     // Useful for some report formats.
-  //     footer: "</checkstyle>"
-  //   }
-  // },
   plugins: [
     new webpack.LoaderOptionsPlugin({
-      debug: true
+      debug: true,
+      options: {
+        resolve: {},
+        tslint: {
+          emitErrors: false,
+          failOnHint: false,
+          resourcePath: 'src',
+          configuration: require('./tslint.json')
+        },
+      }
     }),
     /*
      * Plugin: HtmlWebpackPlugin

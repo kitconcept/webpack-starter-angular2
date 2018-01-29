d41929737b1c87668cee870858ca72a0073ca4e6
Victor Fernandez de Alba <sneridagh@gmail.com>
2018-01-29 17:53:17 +0100

    First working test

---
+++ src/root.js
@@ -0,0 +1,34 @@
+// fetch("http://localhost:8000/api/profile")
+//   .then(response => {
+//     if (response.status !== 200) {
+//       import(/* webpackChunkName: "main" */ "./main");
+//     }
+//     // else {
+//     //   window.location.replace("https://google.com");
+//     // }
+//   })
+//   .catch(e => console.log("error!"));
+
+function component() {
+  var element = document.createElement("div");
+  var button = document.createElement("button");
+  var br = document.createElement("br");
+
+  button.innerHTML = "Click me and look at the console!";
+  element.innerHTML = "Hello webpack";
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e =>
+    import(/* webpackChunkName: "main" */ "./main").then(module => {
+      var main = module.default;
+
+      main();
+    });
+
+  return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
-const path = require('path');
-const webpack = require('webpack');
-const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ContextReplacementPlugin = require("webpack/lib/ContextReplacementPlugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   devServer: {
-    contentBase: 'src',
+    contentBase: "src",
     historyApiFallback: true,
-    host: 'localhost',
+    host: "localhost",
     port: 3000
   },
   entry: {
-    'main'  : './src/main.ts'
+    main: "./src/root.js"
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js"
   },
   resolve: {
-    extensions: ['.ts', '.js', '.json'],
-    modules: ['src', 'node_modules'],
+    extensions: [".ts", ".js", ".json"],
+    modules: ["src", "node_modules"]
   },
   module: {
     loaders: [
-      {test: /.ts$/, loader: 'ts-loader', query: {compilerOptions: {noEmit: false}}},
-    ],
+      {
+        test: /.ts$/,
+        loader: "ts-loader",
+        query: { compilerOptions: { noEmit: false } }
+      }
+    ]
     // noParse: [path.join(__dirname, 'node_modules', 'angular2', 'bundles')]
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "common" // Specify the common bundle's name.
+    }),
     new webpack.LoaderOptionsPlugin({
       debug: true,
       options: {
@@ -35,9 +43,9 @@ module.exports = {
         tslint: {
           emitErrors: false,
           failOnHint: false,
-          resourcePath: 'src',
-          configuration: require('./tslint.json')
-        },
+          resourcePath: "src",
+          configuration: require("./tslint.json")
+        }
       }
     }),
     /*
@@ -49,9 +57,9 @@ module.exports = {
      * See: https://github.com/ampedandwired/html-webpack-plugin
      */
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      chunksSortMode: 'dependency'
-    }),
+      template: "src/index.html",
+      chunksSortMode: "dependency"
+    })
     /**
      * Plugin: ContextReplacementPlugin
      * Description: Provides context to Angular's use of System.import
@@ -59,15 +67,15 @@ module.exports = {
      * See: https://webpack.github.io/docs/list-of-plugins.html#contextreplacementplugin
      * See: https://github.com/angular/angular/issues/11580
      */
-    new ContextReplacementPlugin(
-      // The (\\|\/) piece accounts for path separators in *nix and Windows
-      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-      path.resolve(__dirname, 'src')
-    ),
+    // new ContextReplacementPlugin(
+    //   // The (\\|\/) piece accounts for path separators in *nix and Windows
+    //   /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+    //   path.resolve(__dirname, "src")
+    // )
   ],
   devServer: {
-    contentBase: 'src',
+    contentBase: "src",
     historyApiFallback: true
   },
-  devtool: 'source-map'
+  devtool: "source-map"
 };
